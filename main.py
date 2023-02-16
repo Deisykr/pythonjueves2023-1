a49437c99ff394bdf613ad09b805b5af18d7bf7d
Miguel Angel Ruiz <miguelruiz2205@gmail.com>
2023-02-16 08:16:22 -0500

    nt2 se modifica main con f print y se crea archivo de condicional

---
+++ main.py
@@ -0,0 +1,19 @@
+#datos primitivoso datos basicos, quemados, estamos asignando nosotos mismos el valor
+
+nombreUsuario="Deisy"
+edadUsuario=38
+estaturaUsuario=1.67
+yaDesayuno=True
+
+#el input me permite que el usuario ingrese lo datos
+direccionUsuario=input("Digita tu direccion")
+numero1=int(input("ingrese numero"))
+numero2=int(input("ingrese otro numero"))
+sumatoria=numero1+numero2
+print(f"la suma es:{sumatoria}")
+
+
+#salidas en consola
+print("Hola tu nombre es ",nombreUsuario,"Bienvenido")
+print("tu edad es:",edadUsuario,"estas viejito")
+print(f"tu direcion es:{direccionUsuario} vives cerca")
